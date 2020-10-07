b4c09a246d50316de52921927dcafdea49f767e8
Kaleb Heitzman <kalebheitzman@gmail.com>
2020-10-07 09:19:25 -0400

    autoplay build

--- src/components/Event/Livestream.js
+++ src/components/Event/Livestream.js
@@ -30,7 +30,7 @@ export default ({ mute = false }) => {
 			`}
 		>
 			<iframe 
-				src={`${eventInformation.eventLivestreamUrl}${mute ? "?mute=1" : ""}`} 
+				src={`${eventInformation.eventLivestreamUrl}?mute=1&autoplay=1`} 
 				frameBorder="0" 
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
 				allowFullScreen

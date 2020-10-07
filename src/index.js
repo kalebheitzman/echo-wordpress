7730282bf6600b1e30db8ea790b0df2f12351b72
Kaleb Heitzman <kalebheitzman@gmail.com>
2020-10-07 01:17:40 -0400

    production build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // import libs
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
 // import components
 import App from './components/App'
